be026ad82fe893356d77adc8d02086204c759097
ErvinRivas <ervin.rivas@correounivalle.edu.co>
2022-09-07 18:01:37 -0500

    feat(ervin_rivas): remove parameter /api. RestController

--- src/main/java/com/example/demo/controller/EmpresaController.java
+++ src/main/java/com/example/demo/controller/EmpresaController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class EmpresaController {
 
     private static final  org.slf4j.Logger logger = LoggerFactory.getLogger(EmpresaController.class);
